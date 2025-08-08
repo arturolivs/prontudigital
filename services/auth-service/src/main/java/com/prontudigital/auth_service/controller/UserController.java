89bffb0f17a043a1c8601d8d67dd0fb926412286
Artur Oliveira Santiago <artur.oliveirasantiago@gmail.com>
2025-08-07 23:29:51 -0300

    gateway inicial

--- services/auth-service/src/main/java/com/prontudigital/auth_service/controller/UserController.java
+++ services/auth-service/src/main/java/com/prontudigital/auth_service/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/auth/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
