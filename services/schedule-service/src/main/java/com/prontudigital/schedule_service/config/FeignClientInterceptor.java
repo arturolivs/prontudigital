def57ec78bb384c852e3ebcd262aec809bb1773a
Artur Oliveira Santiago <artur.oliveirasantiago@gmail.com>
2026-03-25 00:14:05 -0300

    Adicionado segurança no servico de agendamentos

--- services/schedule-service/src/main/java/com/prontudigital/schedule_service/config/FeignClientInterceptor.java
+++ services/schedule-service/src/main/java/com/prontudigital/schedule_service/config/FeignClientInterceptor.java
@@ -2,23 +2,20 @@ package com.prontudigital.schedule_service.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 public class FeignClientInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate template) {
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if (attributes != null) {
-            HttpServletRequest request = attributes.getRequest();
-            String authorization = request.getHeader("Authorization");
-            if (authorization != null && !authorization.isEmpty()) {
-                template.header("Authorization", authorization);
-            }
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getCredentials() instanceof Jwt) {
+            Jwt jwt = (Jwt) auth.getCredentials();
+            template.header("Authorization", "Bearer " + jwt.getTokenValue());
         }
     }
 }
