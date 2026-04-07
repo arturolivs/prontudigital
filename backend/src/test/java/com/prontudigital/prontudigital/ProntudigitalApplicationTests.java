9162582be05dd1ab5f820e03a41d45dbd4b13f0a
Artur Oliveira Santiago <artur.oliveirasantiago@gmail.com>
2026-04-07 20:03:14 -0300

    Adicionado projeto backend

---
+++ backend/src/test/java/com/prontudigital/prontudigital/ProntudigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prontudigital.prontudigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntudigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
